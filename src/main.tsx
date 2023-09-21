52b8318e0539d9dccc09bc2f4a915ea94254f073
Sed AKH <kh.seddiqi14@gmail.com>
2023-09-21 11:35:06 +0430

    add the analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
 import myTheme from "./theme.ts";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ColorModeScript initialColorMode={myTheme.config.initialColorMode} />
       <App />
     </ChakraProvider>
+    <Analytics />
   </React.StrictMode>
 );
