395b1149511d3908fba20058000600cdb25a5b2d
Sed AKH <kh.seddiqi14@gmail.com>
2023-09-22 18:54:15 +0430

    Handle game with no image

--- src/services/imageUrL.ts
+++ src/services/imageUrL.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/Noimg.png";
 const getCroppedImageURL = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media".length;
   return url.slice(0, index) + "/crop/600/400/" + url.slice(index);
 };
