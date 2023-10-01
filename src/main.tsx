0a669ac5aa44a2569ff57bcad26c8bb490aa4a1d
Sed AKH <kh.seddiqi14@gmail.com>
2023-10-01 16:12:50 +0430

    removed vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
 import myTheme from "./theme.ts";
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ColorModeScript initialColorMode={myTheme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-    <Analytics />
   </React.StrictMode>
 );
